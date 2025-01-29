ad58ea3458e1ef2355a4ce8275d5d57ed7fd04a1
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-01-29 17:12:00 -0300

    feat: add input new task

--- app/todo-list/page.tsx
+++ app/todo-list/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { FilePenLine } from "lucide-react";
 import { Button } from "../_components/ui/button";
 import {
   Card,
@@ -7,22 +10,37 @@ import {
   CardTitle,
 } from "../_components/ui/card";
 import { Input } from "../_components/ui/input";
+import { Textarea } from "../_components/ui/textarea";
 
 const TodoListPage = () => {
+  const handleAddTask = () => {
+    console.log("Tarefa adicionada com sucesso");
+  };
+
   return (
-    <div className="flex items-center justify-center mt-8">
-      <Card className="w-[350px]">
+    <div className="flex flex-col items-center justify-center mt-8">
+      <h1 className="text-gray-50 text-2xl font-bold mb-6">
+        Seu Gerenciador de Tarefas
+      </h1>
+      <Card className="w-[350px] bg-[#222222]">
         <CardHeader className="text-center">
           <CardTitle>TO DO List</CardTitle>
-          <CardDescription>Crie e gerencie sua tarefas</CardDescription>
+          <CardDescription>Crie e gerencie suas tarefas</CardDescription>
         </CardHeader>
         <CardContent>
           <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-2">
-                <Input id="input-task" placeholder="Adicione uma tarefa" />
-              </div>
-              <Button>Adicionar</Button>
+            <div className="space-y-4">
+              <Input id="input-task" placeholder="Adicione uma tarefa" />
+              <Textarea
+                className="resize-none w-full h-32"
+                placeholder="Insira uma descrição"
+                minLength={2}
+                maxLength={250}
+              />
+              <Button type="button" className="w-full" onClick={handleAddTask}>
+                <FilePenLine size={20} className="mr-2" />
+                Adicionar
+              </Button>
             </div>
           </form>
         </CardContent>
